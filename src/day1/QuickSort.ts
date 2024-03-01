005b731f4b325442be25f8092f55e9ce5396ef17
Tejasva Jaitly <tejasvajaitly@gmail.com>
2024-02-29 17:58:21 -0800

    implement quicksort

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,37 @@
-export default function quick_sort(arr: number[]): void {
+function qs(arr: number[], lo: number, hi: number): void {
+    if (lo >= hi) {
+        return;
+    }
 
+    const pivot = partition(arr, lo, hi);
+    qs(arr, lo, pivot - 1);
+    qs(arr, pivot + 1, hi);
+
+    return;
+}
+
+function partition(arr: number[], lo: number, hi: number): number {
+    const pivot = arr[hi];
+
+    let indx = lo - 1;
+
+    for (let i = lo; i < hi; ++i) {
+        if (arr[i] <= pivot) {
+            indx++;
+            const tmp = arr[i];
+            arr[i] = arr[indx];
+            arr[indx] = tmp;
+        }
+    }
+
+    indx++;
+
+    arr[hi] = arr[indx];
+    arr[indx] = pivot;
+
+    return indx;
+}
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
 }
